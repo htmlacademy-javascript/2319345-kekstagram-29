fe60d5b4856988ef40089822415112c2b89fa181
iskalinina <83082577+iskalinina@users.noreply.github.com>
2023-07-29 15:29:09 +0500

    Перламутровые пуговицы (#12)
    * Added photo sorting
    
    * Replace vendor for build
    
    * Add Vite config
    
    * Fixed sorting
    
    ---------
    
    Co-authored-by: Arthur Baileys Li <arthurlitovko@gmail.com>

--- js/main.js
+++ js/main.js
@@ -4,10 +4,15 @@ import './form-validation.js';
 import './scale-control.js';
 import './effects-control.js';
 import {getData} from './api.js';
-import {showAlert} from './utils.js';
+import {showAlert, debounce} from './utils.js';
+import {init, getSortingPhotos} from './sorting.js';
 
 getData()
-	.then(renderedPhotos)
+	.then((photos) => {
+		const debouncedRenderGallery = debounce(renderedPhotos);
+		init(photos, debouncedRenderGallery);
+		renderedPhotos(getSortingPhotos());
+	})
 	.catch(
 		(err) => {
 			showAlert(err.message);
---
+++ js/sorting.js
@@ -0,0 +1,51 @@
+const Filter = {
+	DEFAULT: 'filter-default',
+	RANDOM: 'filter-random',
+	POPULAR: 'filter-discussed'
+};
+const ACTIVE_BUTTON_CLASS = 'img-filters__button--active';
+const sortingElement = document.querySelector('.img-filters');
+
+let currentFilter = Filter.DEFAULT;
+let photos = [];
+
+const sortInRandom = () => Math.random() - 0.5;
+
+const sortInPopular = (photo1, photo2) =>
+	photo2.comments.length - photo1.comments.length;
+
+const getSortingPhotos = () => {
+	switch (currentFilter) {
+	case Filter.RANDOM:
+		return [...photos].sort(sortInRandom);
+	case Filter.POPULAR:
+		return [...photos].sort(sortInPopular);
+	default:
+		return photos;
+	}
+};
+
+const setOnFilterClick = (cb) => {
+	sortingElement.addEventListener('click', (evt) => {
+		const clickedButton = evt.target;
+		const isButton = clickedButton.classList.contains('img-filters__button');
+		const isCurrentFilter = clickedButton.id === currentFilter;
+
+		if (!isButton || isCurrentFilter) {
+			return;
+		}
+
+		sortingElement.querySelector(`.${ACTIVE_BUTTON_CLASS}`).classList.remove(ACTIVE_BUTTON_CLASS);
+		clickedButton.classList.add(ACTIVE_BUTTON_CLASS);
+		currentFilter = clickedButton.id;
+		cb(getSortingPhotos());
+	});
+};
+
+const init = (loadedPhotos, cb) => {
+	sortingElement.classList.remove('img-filters--inactive');
+	photos = loadedPhotos;
+	setOnFilterClick(cb);
+};
+
+export {init, getSortingPhotos};
